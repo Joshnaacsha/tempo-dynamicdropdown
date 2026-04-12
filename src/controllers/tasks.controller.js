6ce2dcec84717529fb19ff1efa2fbc62b58b0ef0
joshnaacsha <joshnaacsha@gmail.com>
2026-04-12 20:30:17 +0530

    chron: updated controller logic

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -2,29 +2,29 @@ const { getTasksByAccount } = require("../services/mapping.service");
 
 exports.getTasks = (req, res) => {
   try {
-    console.log("Incoming query:", req.query);
+    const accountName = decodeURIComponent(
+      req.query.account ||
+      req.headers["x-tempo-account"] ||
+      ""
+    );
 
- const accountName = decodeURIComponent(
-  req.query.account ||
-  req.headers["x-tempo-account"] ||
-  ""
-);
+    let tasks;
 
-// If no account → return ALL tasks (for Tempo verify)
-if (!accountName) {
-  const allTasks = Object.values(require("../config/mapping.json")).flat();
-  return res.json(allTasks);
-}
+    if (!accountName) {
+      const mapping = require("../config/mapping.json");
+      tasks = Object.values(mapping).flat();
+    } else {
+      tasks = getTasksByAccount(accountName);
+    }
 
-const tasks = getTasksByAccount(accountName);
-res.json(tasks);
-
-    console.log("Tasks returned:", tasks);
-
-    res.json(tasks);
+    res.json({
+      results: tasks.map(t => ({
+        id: t.value,
+        name: t.label
+      }))
+    });
 
   } catch (error) {
-    console.error(error);
-    res.status(500).json([]);
+    res.status(500).json({ results: [] });
   }
 };
