5e7b6cc3e37d3d4cb6f06f950b469c4c61d0be53
joshnaacsha <joshnaacsha@gmail.com>
2026-04-12 20:19:50 +0530

    chron: added vercel.json and updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = require("./src/app");
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Run locally only
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+// Export for Vercel
+module.exports = app;
