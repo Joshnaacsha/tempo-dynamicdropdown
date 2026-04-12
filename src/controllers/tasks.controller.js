ed81bc6c27db5dd535a6b897f1e8e5d9e78db002
joshnaacsha <joshnaacsha@gmail.com>
2026-04-12 20:27:31 +0530

    chron: updated controller logic

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -4,16 +4,20 @@ exports.getTasks = (req, res) => {
   try {
     console.log("Incoming query:", req.query);
 
-    const accountName =
-     decodeURIComponent(
+ const accountName = decodeURIComponent(
   req.query.account ||
   req.headers["x-tempo-account"] ||
   ""
 );
 
-    console.log("Account received:", accountName);
+// If no account → return ALL tasks (for Tempo verify)
+if (!accountName) {
+  const allTasks = Object.values(require("../config/mapping.json")).flat();
+  return res.json(allTasks);
+}
 
-    const tasks = getTasksByAccount(accountName);
+const tasks = getTasksByAccount(accountName);
+res.json(tasks);
 
     console.log("Tasks returned:", tasks);
 
